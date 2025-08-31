82c7e69c0f7a8dd9721d32c76807b869dd2a81ab
Aditya Singh <adityaa9170843@gmail.com>
2025-08-31 17:10:01 +0530

    feat: update .gitignore and enhance prepare.js with Pinecone integration and AI embedding functionality

--- prepare.js
+++ prepare.js
@@ -4,9 +4,31 @@ import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
+import { PineconeStore } from "@langchain/pinecone";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
 dotenv.config({quiet: true});
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+async function main() {
+
+    const ai = new GoogleGenAI({});
+
+    const response = await ai.models.embedContent({
+        model: 'gemini-embedding-001',
+        contents: splits,
+    });
+
+    console.log(response.embeddings);
+}
+
+main();
+
+
+const pinecone = new PineconeClient();
+const vectorStore = new PineconeStore(embeddings, {
+  pineconeIndex,
+  maxConcurrency: 5,
+});
 
 
 export async function indexTheDocument(filePath) {
