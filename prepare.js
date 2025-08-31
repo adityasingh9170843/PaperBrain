cd82de18b8efa4ee88dc356065a6d94c31bc8ede
Aditya Singh <adityaa9170843@gmail.com>
2025-08-31 17:06:26 +0530

    feat: add Google GenAI integration and update dependencies

--- prepare.js
+++ prepare.js
@@ -1,7 +1,25 @@
+process.removeAllListeners("warning");
+process.on("warning", () => {});
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+dotenv.config({quiet: true});
 
-export async function indexTheDocument(filePath){
-    const loader = new PDFLoader(filePath);
-    const docs = await loader.load();
-    console.log(docs);
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+
+export async function indexTheDocument(filePath) {
+  const loader = new PDFLoader(filePath, { splitPages: false });
+  const docs = await loader.load();
+  
+
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 100,
+  });
+
+  const splits = await textSplitter.splitText(docs[0].pageContent);
+  console.log(splits.length);
+  
 }
