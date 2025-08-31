942d22d88262bd2c493fe489fd0adccf96f5cb37
Aditya Singh <adityaa9170843@gmail.com>
2025-08-31 16:16:40 +0530

    Refactor code structure for improved readability and maintainability

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from "express";
-
+import { indexTheDocument } from "./prepare.js";
 
 
 const app = express();
+
+
+
+const filePath = "./sample.pdf";
+indexTheDocument(filePath);
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
