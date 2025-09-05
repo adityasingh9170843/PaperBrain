4fd2d3941f7c20468efc579cef2a1fb20d0709d5
Aditya Singh <adityaa9170843@gmail.com>
2025-09-05 22:57:58 +0530

    Initialize frontend project with Vite and React setup
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG to public directory.
    - Implemented basic App component structure in App.jsx.
    - Included React logo SVG in assets directory.
    - Created index.css file for global styles.
    - Set up main entry point in main.jsx to render the App component.
    - Configured Vite with react plugin in vite.config.js.

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 import express from "express";
 import { indexTheDocument } from "./prepare.js";
-
+import path from "path";
+import fs from "fs";
+import multer from "multer";
 
 const app = express();
 
+const upload = multer({dest: 'uploads/'});
 
 
-const filePath = "./sample.pdf";
-indexTheDocument(filePath);
+app.post('/upload',upload.single.single("pdf"),async(req,res)=>{
+    try{
+        if(!req.file){
+            return res.status(400).send("No file uploaded");
+        }
+
+        const filePath = path.join("uploads",req.file.filename);
+        await indexTheDocument(filePath);
+        res.status(200).send("File uploaded and indexed successfully");
+    }catch(err){
+        console.log(err);
+        res.status(500).send("Error uploading file");
+    }
+})
+
+
 
 
 
